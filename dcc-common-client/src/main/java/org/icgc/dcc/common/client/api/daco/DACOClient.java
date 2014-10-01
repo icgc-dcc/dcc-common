ae03a12411aa34ba569cc0d945e0dbe2e2260e5b
Vitalii Slobodianyk <vitalii.slobodianyk@oicr.on.ca>
2014-10-01 09:34:45 -0400

    DACO documentation update

--- dcc-common-client/src/main/java/org/icgc/dcc/common/client/api/daco/DACOClient.java
+++ dcc-common-client/src/main/java/org/icgc/dcc/common/client/api/daco/DACOClient.java
@@ -24,10 +24,13 @@ import java.util.NoSuchElementException;
  * A client to query DACO API. To get a DACO access it is required to fill-in the DACO application. For more information
  * check <a href="https://icgc.org/daco">How to get DACO access</a>.<br>
  * <br>
- * DACO access is granted to OpenID accounts only. In other words, if a user has an ICGC account they also need an
- * OpenID account. However, the DACO API does not check if the account is valid or not. Thus, it's possible to use just
- * a random unique URI. Multiple ICGC accounts can share a single OpenID account. It is unknown if an ICGC account can
- * be used as the OpenID account.
+ * DACO access is granted to OpenID accounts only. In other words, if a user has an ICGC(CUD) account they also need an
+ * OpenID account to get the DACO access. However, the DACO API does not check if the account is valid or not. Thus,
+ * it's possible to use just a random unique URI.<br>
+ * <br>
+ * Multiple ICGC(CUD) accounts can share a single OpenID account. Also, they may be associated with more than one OpenID
+ * account. Importantly, if the application associated to that OpenID is not approved, then the OpenID has no
+ * authorization power into DACO. However, such a behaviour is unusual.
  */
 public interface DACOClient {
 
@@ -68,8 +71,8 @@ public interface DACOClient {
   /**
    * Returns filtered approved DACO users.
    * 
-   * @param userType - type of search (by OpenID or by username)
-   * @param userValue - OpenID or username being searched
+   * @param userType - type of search (by OpenID or by CUD)
+   * @param userValue - OpenID or CUD being searched
    * @throws NoSuchElementException
    * @see <a href="https://wiki.oicr.on.ca/pages/viewpage.action?pageId=57773222">Search - One DACO Approved Users by
    * entity-filter (v2.0)</a>
@@ -77,11 +80,11 @@ public interface DACOClient {
   List<User> getUsersByType(UserType userType, String userValue);
 
   /**
-   * Checks if {@code userId} is in the list of the approved DACO users. The {@code id} may be either an openId or a
-   * username.
+   * Checks if {@code userId} is in the list of the approved DACO users. The {@code userId} may be either an
+   * {@code OPENID} or a {@code CUD}.
    * 
-   * @param userId to be checked. The user should pass user's username if the method is invoked for {@link UserType.CUD}
-   * or user's openID in case of {@link UserType.OPENID}
+   * @param userId to be checked. The user should pass user's {@code username} if the method is invoked for {@code CUD}
+   * or user's openID in case of {@code OPENID}
    * @return <b>true</b> if the {@code userId} is approved, otherwise - <b>false</b>
    */
   boolean hasDacoAccess(String userId, UserType userType);
