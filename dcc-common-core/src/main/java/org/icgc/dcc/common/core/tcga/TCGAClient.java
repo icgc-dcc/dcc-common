cd6aa1d14194462c24f2ba9670990d4d908bf7e1
Bob Tiernay <btiernay@oicr.on.ca>
2015-05-05 16:53:50 -0400

    Added batch support to TCGA client.

--- dcc-common-core/src/main/java/org/icgc/dcc/common/core/tcga/TCGAClient.java
+++ dcc-common-core/src/main/java/org/icgc/dcc/common/core/tcga/TCGAClient.java
@@ -17,9 +17,19 @@
  */
 package org.icgc.dcc.common.core.tcga;
 
+import static com.google.common.collect.Iterables.partition;
+import static com.google.common.net.HttpHeaders.CONTENT_LENGTH;
+import static com.google.common.net.HttpHeaders.CONTENT_TYPE;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.icgc.dcc.common.core.util.Jackson.DEFAULT;
+import static org.icgc.dcc.common.core.util.Joiners.COMMA;
 
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -27,6 +37,7 @@ import lombok.SneakyThrows;
 import lombok.val;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.collect.ImmutableMap;
 
 /**
  * See https://wiki.nci.nih.gov/display/TCGA/TCGA+Barcode+to+UUID+Web+Service+User%27s+Guide
@@ -38,6 +49,7 @@ public class TCGAClient {
    * Constants.
    */
   private static final String TCGA_BASE_URL = "https://tcga-data.nci.nih.gov";
+  private static final int TCGA_MAX_BATCH_SIZE = 500;;
 
   @NonNull
   private final String baseUrl;
@@ -55,6 +67,15 @@ public class TCGAClient {
   }
 
   @NonNull
+  public Map<String, String> getUUIDs(Set<String> barcodes) {
+    return getIDs(
+        getBarcodesMappingsURL(),
+        barcodes,
+        (JsonNode mapping) -> mapping.get("barcode").textValue(),
+        (JsonNode mapping) -> mapping.get("uuid").textValue());
+  }
+
+  @NonNull
   public String getBarcode(String uuid) {
     val mappingUrl = getUUIDMappingURL(uuid);
     val mapping = getMapping(mappingUrl);
@@ -62,12 +83,51 @@ public class TCGAClient {
     return getMappingBarcode(mapping);
   }
 
+  @NonNull
+  public Map<String, String> getBarcodes(Set<String> uuids) {
+    return getIDs(
+        getUUIDMappingsURL(),
+        uuids,
+        (JsonNode mapping) -> mapping.get("uuid").textValue(),
+        (JsonNode mapping) -> mapping.get("barcode").textValue());
+  }
+
+  public Map<String, String> getIDs(String mappingsUrl, Set<String> ids, Function<JsonNode, String> key,
+      Function<JsonNode, String> value) {
+    val map = ImmutableMap.<String, String> builder();
+    for (val batch : getBatches(ids)) {
+      val mappings = getMappings(mappingsUrl, batch);
+      val node = mappings.path("uuidMapping");
+      if (node.isArray()) {
+        for (val mapping : node) {
+          map.put(key.apply(mapping), value.apply(mapping));
+        }
+      } else {
+        map.put(key.apply(node), value.apply(node));
+      }
+    }
+
+    return map.build();
+  }
+
+  private static Iterable<List<String>> getBatches(Set<String> barcodes) {
+    return partition(barcodes, TCGA_MAX_BATCH_SIZE);
+  }
+
   private String getBarcodeMappingURL(String barcode) {
     return getMappingURL("/barcode" + "/" + barcode);
   }
 
+  private String getBarcodesMappingsURL() {
+    return getMappingURL("/barcode/batch");
+  }
+
   private String getUUIDMappingURL(String uuid) {
     return getMappingURL("/uuid" + "/" + uuid);
+  }
+
+  private String getUUIDMappingsURL() {
+    return getMappingURL("/uuid/batch");
   }
 
   private String getMappingURL(String path) {
@@ -85,6 +145,19 @@ public class TCGAClient {
   @SneakyThrows
   private static JsonNode getMapping(String url) {
     return DEFAULT.readTree(new URL(url));
+  }
+
+  @SneakyThrows
+  private static JsonNode getMappings(String url, Iterable<String> ids) {
+    val body = COMMA.join(ids);
+    val connection = (HttpURLConnection) new URL(url).openConnection();
+    connection.setRequestMethod("POST");
+    connection.setRequestProperty(CONTENT_TYPE, "text/plain");
+    connection.setRequestProperty(CONTENT_LENGTH, Integer.toString(body.length()));
+    connection.setDoOutput(true);
+    connection.getOutputStream().write(body.getBytes(UTF_8));
+
+    return DEFAULT.readTree(connection.getInputStream());
   }
 
 }
