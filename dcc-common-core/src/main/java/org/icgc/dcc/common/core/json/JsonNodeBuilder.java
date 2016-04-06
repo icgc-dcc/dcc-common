3a3cd9f02ab84d3ecac8aeccacdc477f5243af3e
btiernay <btiernay@oicr.on.ca>
2016-04-06 17:29:50 -0400

    Updated JsonNodeBuilder to be more complete.

--- dcc-common-core/src/main/java/org/icgc/dcc/common/core/json/JsonNodeBuilder.java
+++ dcc-common-core/src/main/java/org/icgc/dcc/common/core/json/JsonNodeBuilder.java
@@ -36,33 +36,52 @@ public interface JsonNodeBuilder<J extends JsonNode> {
   J end();
 
   /**
-   * Factory method for an {@link ObjectNode} builder.
+   * Factory methods for an {@link ObjectNode} builder.
    */
+
   public static ObjectNodeBuilder object() {
     return new ObjectNodeBuilder(JsonNodeFactory.instance);
   }
 
-  public static ObjectNodeBuilder object(String k1, String v1) {
+  public static ObjectNodeBuilder object(@NonNull String k1, String v1) {
     return object().with(k1, v1);
   }
 
-  public static ObjectNodeBuilder object(String k1, int v1) {
+  public static ObjectNodeBuilder object(@NonNull String k1, String v1, @NonNull String k2, String v2) {
+    return object(k1, v1).with(k2, v2);
+  }
+
+  public static ObjectNodeBuilder object(@NonNull String k1, String v1, @NonNull String k2, String v2,
+      @NonNull String k3, String v3) {
+    return object(k1, v1, k2, v2).with(k3, v3);
+  }
+
+  public static ObjectNodeBuilder object(@NonNull String k1, int v1) {
     return object().with(k1, v1);
   }
 
-  public static ObjectNodeBuilder object(String k1, float v1) {
+  public static ObjectNodeBuilder object(@NonNull String k1, float v1) {
     return object().with(k1, v1);
+  }
+
+  public static ObjectNodeBuilder object(@NonNull String k1, JsonNodeBuilder<?> builder) {
+    return object().with(k1, builder);
   }
 
   /**
-   * Factory method for an {@link ArrayNode} builder.
+   * Factory methods for an {@link ArrayNode} builder.
    */
+
   public static ArrayNodeBuilder array() {
     return new ArrayNodeBuilder(JsonNodeFactory.instance);
   }
 
-  public static ArrayNodeBuilder array(String... values) {
+  public static ArrayNodeBuilder array(@NonNull String... values) {
     return array().with(values);
+  }
+
+  public static ArrayNodeBuilder array(@NonNull JsonNodeBuilder<?>... builders) {
+    return array().with(builders);
   }
 
   final class ObjectNodeBuilder implements JsonNodeBuilder<ObjectNode> {
@@ -107,6 +126,10 @@ public interface JsonNodeBuilder<J extends JsonNode> {
       return this;
     }
 
+    public ObjectNodeBuilder with(@NonNull String field, JsonNodeBuilder<?> builder) {
+      return with(field, builder.end());
+    }
+
     public ObjectNodeBuilder with(@NonNull String field, String value) {
       return with(field, factory.textNode(value));
     }
@@ -140,17 +163,37 @@ public interface JsonNodeBuilder<J extends JsonNode> {
       return this;
     }
 
+    public ArrayNodeBuilder with(JsonNode node, JsonNode... nodes) {
+      with(node);
+      for (val n : nodes) {
+        with(n);
+      }
+      return this;
+    }
+
+    public ArrayNodeBuilder with(JsonNodeBuilder<?> node) {
+      return with(node);
+    }
+
+    public ArrayNodeBuilder with(JsonNodeBuilder<?>... builders) {
+      for (val value : builders) {
+        with(value);
+      }
+
+      return this;
+    }
+
     public ArrayNodeBuilder with(String value) {
       thisNode.add(value);
       return this;
     }
 
-    public ArrayNodeBuilder with(Integer value) {
+    public ArrayNodeBuilder with(int value) {
       thisNode.add(value);
       return this;
     }
 
-    public ArrayNodeBuilder with(Float value) {
+    public ArrayNodeBuilder with(float value) {
       thisNode.add(value);
       return this;
     }
@@ -163,7 +206,7 @@ public interface JsonNodeBuilder<J extends JsonNode> {
       return this;
     }
 
-    public ArrayNodeBuilder with(Iterable<String> values) {
+    public ArrayNodeBuilder with(@NonNull Iterable<String> values) {
       for (val value : values) {
         thisNode.add(value);
       }
